b25a8cc9efcd1b9025b408a91709ae060abad661
Gecko05 <jacacent@gmail.com>
2017-07-26 22:22:46 -0500

    Weather Update

--- Gwent.py
+++ Gwent.py
@@ -2,19 +2,29 @@ from GwentDatabase import *
 from GwentScript import *
 
 Game = GameBoard()
+
 myHand = Hand([])
 myDeck = Deck([AncientFoglet,Wyvern,AncientFoglet])
 myDeck.draw(myHand,2)
+
 enemyDeck = Deck([AlzursThunder,Wyvern,Wyvern])
 enemyHand = Hand([])
 enemyDeck.draw(enemyHand,3)
+
 myHand.use(Game,myHand.cards[0],0,'none','r')
+
 enemyHand.use(Game,enemyHand.cards[0],1,Game.board[0]['r'][0])
 enemyHand.use(Game,enemyHand.cards[0],1)
 enemyHand.use(Game,enemyHand.cards[0],1)
+
 Game.display()
-Game.board[1]['s'][0].deployUnit(Game.board[0]['r'][0])
-Game.board[1]['s'][1].deployUnit(Game.board[0]['r'][0])
+
+Game.board[1]['s'][0]['unit'].deployUnit(Game.board[0]['r'][0])
+Game.board[1]['s'][1]['unit'].deployUnit(Game.board[0]['r'][0])
+
+TorrentialRain.cast(Game,0,'1s')
+
 Game.display()
 Game.update()
+
 print(Game.score)
--- GwentScript.py
+++ GwentScript.py
@@ -11,7 +11,7 @@ class Unit:
     def deployUnit(self,target = False):                                #DEPLOY ABILITY FOR CARD
         '''target optional'''
         if(self.deploy[0])=='d' and target!= False:
-            target.power = target.power - int(self.deploy[1])
+            target['power'] = target['power'] - int(self.deploy[1])
 
 class Special:
     def __init__(self,name,ability):
@@ -25,13 +25,14 @@ class Card:
         self.special = special
     def cast(self,game,player,target):                                  #CAST FOR SPECIAL CARDS
         if(self.unit.ability[0])=='d' and target!= False:
-            target.power = target.power - int(self.unit.ability[1])
-        
+            target['power'] = target['power'] - int(self.unit.ability[1])
+        elif(self.unit.ability[0])=='w' and target!=False:
+            game.weather[int(target[0])][target[1]] = self.unit.ability[1]  #CAST WEATHER
     def summon(self,game,player,row):                                   #SUMMON UNIT FROM CARD
         if(self.unit.row=='a'):
-            game.board[player][row].append(self.unit)
+            game.board[player][row].append({'name':self.unit.name,'power':self.unit.power,'unit':self.unit})
         else:
-            game.board[player][self.unit.row].append(self.unit)
+            game.board[player][self.unit.row].append({'name':self.unit.name,'power':self.unit.power,'unit':self.unit})
 
 
 #0 is self
@@ -39,30 +40,60 @@ class Card:
 class GameBoard:
     def __init__(self):
         self.board = [{"s":[],"r":[],"m":[]},{"s":[],"r":[],"m":[]}]
+        self.weather = [{"s":0,"r":0,"m":0},{"s":0,"r":0,"m":0}]
         self.score = [0,0]
         self.played = {0:[],1:[]}
         self.graveyard = {0:[],1:[]}
     def change(self,board,player):
         self.board[player] = board
+    def tick(self):
+        for side in self.board:
+            for row in side:
+                if(self.weather[self.board.index(side)][row]=='r'):             #TORRENTIAL RAIN WEATHER
+                    minunits = self.getRowMax(self.board.index(side),row)
+                    if(minunits!=[]):
+                        for unitpos in minunits:
+                            self.board[self.board.index(side)][row][unitpos]['power'] = self.board[self.board.index(side)][row][unitpos]['power']-1
+        
     def update(self):                        #UPDATE DESTROYED UNITS
         for side in self.board:
             for row in side:
                 for unit in side[row]:
-                    if(unit.power<=0):
-                        unit.power = unit.base
+                    if(unit['power']<=0):
                         self.graveyard[self.board.index(side)].append(side[row].pop(side[row].index(unit)))
 
         self.score = [0,0]                   #UPDATE SCORE
         for side in self.board:
             for row in side:
                 for unit in side[row]:
-                    self.score[self.board.index(side)] = self.score[self.board.index(side)] + unit.power
+                    self.score[self.board.index(side)] = self.score[self.board.index(side)] + unit['power']
 
     def display(self):                      #DISPLAY THE CURRENT BOARD
         for side in self.board:
             for row in side:
                 for unit in side[row]:
-                    print(unit.name + ' ' + str(unit.power))
+                    print(unit['name'] + ' ' + str(unit['power']))
+
+    def getRowMax(self,side,row):
+        maxUnit = max(self.board[side][row],key=lambda x:x['power'])['power'] #GET MAX POWER ON THE ROW
+        units = []
+        auxRow = self.board[side][row][:]
+        for unit in auxRow:
+            if(unit['power']) == maxUnit:
+                units.append(auxRow.index(unit))
+                auxRow[auxRow.index(unit)] = 0
+        return units
+    
+    def getRowMin(self,side,row):
+        minUnit = min(self.board[side][row],key=lambda x:x['power'])['power'] #GET MIN POWER ON THE ROW
+        units = []
+        auxRow = self.board[side][row][:]
+        for unit in auxRow:
+            if(unit['power']) == minUnit:
+                units.append(auxRow.index(unit))
+                auxRow[auxRow.index(unit)] = 0
+        return units
+            
 
 class Hand:
     def __init__(self,cards):
